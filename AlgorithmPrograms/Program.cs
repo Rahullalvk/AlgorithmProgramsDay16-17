0ad466e0c50e1c7b69472ca79ae02260d2eb1991
Rahulalvk <rahulal432@gmail.com>
2023-05-18 22:18:57 -0700

    Bubble Sort

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -1,25 +1,50 @@
-﻿namespace AlgorithmPrograms
+﻿namespace AlgorithmPrograms 
+{  
+
+class BubbleSort
 {
-    class InsertionSort
+    static void Main()
     {
-        static void Main()
+        Console.WriteLine("Enter the integers :");
+
+        var numbers = new List<int>();
+        string input;
+        while (!string.IsNullOrEmpty(input = Console.ReadLine()))
         {
-            Console.WriteLine("Enter a list of words (separated by spaces):");
-            string input = Console.ReadLine();
-
-            string[] words = input.Split(' ');
-
-            Sorter sorter = new Sorter();
-
-            sorter.Sort(words);
-
-            Console.WriteLine("Sorted List:");
-            foreach (string word in words)
+            if (int.TryParse(input, out int number))
             {
-                Console.WriteLine(word);
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
             }
         }
-    } 
-}
 
-    
+        BubbleSortAscending(numbers);
+
+        Console.WriteLine("Sorted List:");
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    static void BubbleSortAscending(List<int> numbers)
+    {
+        int n = numbers.Count;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+}
