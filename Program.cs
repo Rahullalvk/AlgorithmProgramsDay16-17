1c9dd9af88c212c8add592bf0afd6b2d6d08fe99
Rahulalvk <rahulal432@gmail.com>
2023-05-18 23:10:15 -0700

    prime number

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+class PrimeNumberFinder
+{
+    static void Main()
+    {
+        Console.WriteLine("Prime numbers between 0 and 1000:");
+        FindPrimeNumbers(0, 1000);
+    }
+
+    static void FindPrimeNumbers(int start, int end)
+    {
+        for (int number = start; number <= end; number++)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
